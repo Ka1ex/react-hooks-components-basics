2d1ff476dc0af4e013850d3c847f46a0a01e2041
ka1ex <kalexadamson@hotmail.com>
2021-06-07 18:19:31 -0500

    commit tree 5ec41f940a878d004c1e70f71539b02fc4c1fcc4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import NateComponent from "./NateComponent";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <NateComponent />
     </div>
   );
 }
---
+++ src/NateComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NateComponent = () => {
+    return <div>This is my comment</div>
+}
+
+export default NateComponent;
